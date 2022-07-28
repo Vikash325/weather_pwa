f429756ad1757d7d8a134db1420e2b5fe950cb8e
Vikash325 <109344449+Vikash325@users.noreply.github.com>
2022-07-28 19:02:49 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      console.log(data);
+      setQuery("");
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Enter your City..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+            <div className="other-info">
+              <div className="city_info">
+                <label className="other_label">Maximum Temp</label>
+                <p>{weather.main.temp_max}</p>
+              </div>
+              <div className="city_info">
+                <label className="other_label">Humidity</label>
+                <p>{weather.main.humidity}</p>
+              </div>
+              <div className="city_info">
+                <label className="other_label">Minimum Temp</label>
+                <p>{Math.round(weather.main.temp_min)}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
